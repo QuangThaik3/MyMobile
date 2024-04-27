951fc2fae702de9832487a398e0716fc974a932c
QuangThaik3 <134220911+QuangThaik3@users.noreply.github.com>
2024-04-27 15:14:12 +0700

    Update index.router.js

--- src/v1/routes/index.router.js
+++ src/v1/routes/index.router.js
@@ -8,4 +8,25 @@ router.get('/checkstatus', (req, res, next) => {
     })
 })
 
+router.get('/checkstatus', (req, res, next) => {
+    res.status(200).json({
+        status: 'success api',
+        message: 'api ok',
+        metadata: [
+            {
+                name: 'anonystick',
+                age: 40
+            },
+            {
+                name: 'rolando',
+                age: 39
+            },
+            {
+                name: 'messi',
+                age: 37
+            },
+        ]
+    })
+})
+
 module.exports = router;
